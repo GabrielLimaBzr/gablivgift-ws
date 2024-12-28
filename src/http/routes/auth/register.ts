1d4cb11b822fff8659aa4c6dc9feb636c65101ad
gabriel.bezerra <gabriel18lb@outlook.com>
2024-12-28 18:01:55 -0300

    fixes

--- src/http/routes/auth/register.ts
+++ src/http/routes/auth/register.ts
@@ -1,38 +1,86 @@
 import { FastifyInstance } from 'fastify';
 import bcrypt from 'bcryptjs';
 import { PrismaClient } from '@prisma/client';
+import nodemailer from 'nodemailer';
+import { z } from 'zod'
 
 const prisma = new PrismaClient();
 
+const email = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  secure: true,
+  port: 465,
+  auth: {
+    user: process.env.EMAIL_SENDER,
+    pass: process.env.EMAIL_PASS
+  }
+});
+
 export async function register(fastify: FastifyInstance) {
+  //Validação
+  const dto = z.object({
+    fullName: z
+      .string()
+      .min(3, 'O  nome deve ter pelo menos 3 caracteres')
+      .max(100, 'O nome não pode exceder 100 caracteres'),
+
+    email: z
+      .string().email('Email invalido!').min(3, 'Email invalido!'),
+
+    password: z
+      .string().min(8, "A senha deve ter no mínimo 8 caracteres")
+  });
+
+
+  //Rota
   fastify.post('/register', async (request, reply) => {
-    const { fullName, email, password } = request.body as { fullName: string; email: string; password: string };
 
-    // Verifica se o usuário já existe
-    const existingUser = await prisma.user.findUnique({
-      where: { email },
-    });
+    try {
 
-    if (existingUser) {
-      return reply.status(400).send({ message: 'Usuário já existe!' });
-    }
-
-    // Criptografa a senha
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    // Cria o novo usuário no banco de dados
-    const user = await prisma.user.create({
-      data: {
+      const {
         fullName,
         email,
-        password: hashedPassword,
-      },
-      select: {
-        id: true,
-        fullName: true,
-      },
-    });
+        password
+      } = dto.parse(request.body);
 
-    reply.status(201).send({ message: 'Usuário criado com sucesso!', user });
+
+      // Verifica se o usuário já existe
+      const existingUser = await prisma.user.findUnique({
+        where: { email },
+      });
+
+      if (existingUser) {
+        return reply.status(400).send({ message: 'Usuário já existe!' });
+      }
+
+      // Criptografa a senha
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      // Cria o novo usuário no banco de dados
+      const user = await prisma.user.create({
+        data: {
+          fullName,
+          email,
+          password: hashedPassword,
+        },
+        select: {
+          id: true,
+          fullName: true,
+          isActive: true,
+        },
+      });
+
+      reply.status(201).send({ message: 'Usuário criado com sucesso!', user });
+
+
+    } catch (err) {
+      if (err instanceof z.ZodError) {
+        return reply.status(400).send({ error: err.errors });
+      }
+      return reply.status(500).send({ error: 'Erro interno do servidor' });
+
+    }
+
+
   });
 }
--- src/http/routes/gift/create-gift.ts
+++ src/http/routes/gift/create-gift.ts
@@ -1,69 +1,68 @@
 import { FastifyInstance } from 'fastify'
 import { z } from 'zod'
-import { prisma } from '../../lib/prisma'
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 export async function createGift(app: FastifyInstance) {
-    // Definindo o esquema de validação de forma mais compacta
-    const createGiftSchema = z.object({
-      title: z
-        .string()
-        .min(3, 'O título deve ter pelo menos 3 caracteres')
-        .max(100, 'O título não pode exceder 100 caracteres'),
-      description: z
-        .string()
-        .min(3, 'A descrição deve ter pelo menos 3 caracteres')
-        .max(500, 'A descrição não pode exceder 500 caracteres'),
-      estimatedPrice: z
-        .number()
-        .positive('O preço estimado deve ser um número positivo')
-        .max(100000, 'O preço estimado não pode exceder 100.000'),
-      category: z
-        .number()
-        .int('A categoria deve ser um número inteiro')
-        .optional(),
-      priority: z.boolean().optional(),
-      addedByUserId: z.number(),
-      imageUrl: z.string().url().optional(),
-      coupleId: z.number().optional(),
-    });
-  
-    app.post('/create-gift', async (request, reply) => {
-      try {
-        // Validando os dados recebidos
-        const {
+  const createGiftSchema = z.object({
+    title: z
+      .string()
+      .min(3, 'O título deve ter pelo menos 3 caracteres')
+      .max(100, 'O título não pode exceder 100 caracteres'),
+    description: z
+      .string()
+      .min(3, 'A descrição deve ter pelo menos 3 caracteres')
+      .max(500, 'A descrição não pode exceder 500 caracteres'),
+    estimatedPrice: z
+      .number()
+      .positive('O preço estimado deve ser um número positivo')
+      .max(100000, 'O preço estimado não pode exceder 100.000'),
+    category: z
+      .number()
+      .int('A categoria deve ser um número inteiro')
+      .optional(),
+    priority: z.boolean().optional(),
+    addedByUserId: z.number(),
+    imageUrl: z.string().url().optional(),
+    coupleId: z.number().optional(),
+  });
+
+  app.post('/create-gift', async (request, reply) => {
+    try {
+      const {
+        title,
+        description,
+        estimatedPrice,
+        category = 9,
+        priority = false,
+        addedByUserId,
+        imageUrl,
+        coupleId,
+      } = createGiftSchema.parse(request.body);
+
+      const gift = await prisma.gift.create({
+        data: {
           title,
           description,
           estimatedPrice,
-          category = 9,
-          priority = false,
+          category,
+          priority,
           addedByUserId,
           imageUrl,
           coupleId,
-        } = createGiftSchema.parse(request.body);
-  
-        // Inserindo o novo gift no banco de dados
-        const gift = await prisma.gift.create({
-          data: {
-            title,
-            description,
-            estimatedPrice,
-            category,
-            priority,
-            addedByUserId,
-            imageUrl,
-            coupleId,
-          },
-        });
-  
-        // Retornando a resposta com o gift criado
-        return reply.status(201).send({ gift });
-      } catch (err) {
-        if (err instanceof z.ZodError) {
-          // Erro de validação
-          return reply.status(400).send({ error: err.errors });
-        }
-        // Erro interno do servidor
-        return reply.status(500).send({ error: 'Erro interno do servidor' });
+        },
+      });
+
+      // Retornando a resposta com o gift criado
+      return reply.status(201).send({ gift });
+    } catch (err) {
+      if (err instanceof z.ZodError) {
+        // Erro de validação
+        return reply.status(400).send({ error: err.errors });
       }
-    });
-  }
+      // Erro interno do servidor
+      return reply.status(500).send({ error: 'Erro interno do servidor' });
+    }
+  });
+}
