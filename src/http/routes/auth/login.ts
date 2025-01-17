3564d7e60dbfe412c3b46f36871764dabde8b5b5
gabriel.bezerra <gabriel18lb@outlook.com>
2025-01-17 18:19:26 -0300

    remove redis

--- src/http/routes/auth/login.ts
+++ src/http/routes/auth/login.ts
@@ -1,7 +1,6 @@
 import { FastifyInstance } from 'fastify';
 import bcrypt from 'bcryptjs';
 import { PrismaClient } from '@prisma/client';
-import redis from '../../../redis';
 
 
 const prisma = new PrismaClient();
@@ -16,7 +15,6 @@ export async function login(fastify: FastifyInstance) {
         return reply.status(400).send({ message: 'Email e senha são obrigatórios!' });
       }
 
-      // Verifica se o usuário existe
       const user = await prisma.user.findUnique({
         where: { email },
       });
@@ -24,17 +22,18 @@ export async function login(fastify: FastifyInstance) {
       if (!user) {
         return reply.status(400).send({ message: 'Usuário ou senha incorretos!' });
       }
+      
+      if (!user.isActive) {
+        return reply.status(403).send({ message: 'Usuário inativo ou não verificado!' });
+      }
 
-      // Verifica se a senha está correta
       const isPasswordValid = await bcrypt.compare(password, user.password);
 
       if (!isPasswordValid) {
         return reply.status(400).send({ message: 'Usuário ou senha incorretos!' });
       }
 
-      if (!user.isActive) {
-        return reply.status(403).send({ message: 'Usuário inativo ou não verificado!' });
-      }
+      
 
       // Recupera o casal associado ao usuário (se existir)
       const couple = await prisma.couple.findFirst({
@@ -52,25 +51,11 @@ export async function login(fastify: FastifyInstance) {
         email: user.email,
       };
 
-      // Verifica se já existe um token válido no Redis
-      const cachedToken = await redis.get(`user:${user.id}:token`);
-      if (cachedToken) {
-        return reply.send({
-          message: 'Login bem-sucedido!',
-          token: cachedToken,
-          user: userResponse,
-          couple,
-        });
-      }
-
       // Gera o token JWT com validade de 7 dias
       const token = fastify.jwt.sign({
         userId: user.id,
-        coupleId: couple?.id || null,
+        
       });
-
-      // Armazena o token no Redis com expiração automática
-      await redis.set(`user:${user.id}:token`, token, 'EX', 7 * 24 * 60 * 60); // Expira em 7 dias
 
       return reply.send({
         message: 'Login bem-sucedido!',
--- src/redis.ts
+++
@@ -1,8 +0,0 @@
-import Redis from 'ioredis';
-
-const redis = new Redis({
-  host: 'localhost', 
-  port: 6379,        // Porta padrão do Redis
-});
-
-export default redis;
